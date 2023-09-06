d21b99831594b38dfc21766884944d4e9d7dcc8e
MariaWerlang <maria.werlang@etec.sp.gov.br>
2023-09-06 14:34:37 -0300

    First Version

---
+++ app/src/main/java/com/example/aulajetpack/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.aulajetpack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val darkGreen = Color(0XFF8AC926)
+val darkBlue = Color(0XFF0077B6)
+val darkOrange = Color(0XFFFEE440)
+val darkRed = Color(0XFFC1121F)
